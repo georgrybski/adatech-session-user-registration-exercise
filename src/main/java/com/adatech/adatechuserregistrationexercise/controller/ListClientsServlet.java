ded21caa4d557fce8bae6a2b6d647dac0e131458
georgrybski <rybskigeorg@gmail.com>
2023-02-20 21:58:43 -0300

    Added Client class

---
+++ src/main/java/com/adatech/adatechuserregistrationexercise/controller/ListClientsServlet.java
@@ -0,0 +1,21 @@
+package com.adatech.adatechuserregistrationexercise.controller;
+
+import com.adatech.adatechuserregistrationexercise.ImprovisedEnumInjector;
+import com.adatech.adatechuserregistrationexercise.service.UserService;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+
+@WebServlet(name = "ListClientsServlet", value = "/list-registered-clients")
+public class ListClientsServlet extends BaseServlet {
+
+    private UserService userService = ImprovisedEnumInjector.USER_SERVICE.getUserService();
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("content", userService.getAllClientsHtmlContent());
+        request.getRequestDispatcher(jspDirectory + "list-registered-clients.jsp").forward(request, response);
+    }
+}
---
+++ src/main/java/com/adatech/adatechuserregistrationexercise/model/Client.java
@@ -0,0 +1,13 @@
+package com.adatech.adatechuserregistrationexercise.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Client {
+    private String name;
+    private String cpf;
+}
+
+
