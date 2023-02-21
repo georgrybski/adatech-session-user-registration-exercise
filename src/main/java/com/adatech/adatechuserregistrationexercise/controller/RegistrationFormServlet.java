8fc582ccc1ec41ab6f297c1b26be8f9e14682a53
georgrybski <rybskigeorg@gmail.com>
2023-02-20 22:03:11 -0300

    Added RegistrationFormServlet

---
+++ src/main/java/com/adatech/adatechuserregistrationexercise/controller/RegistrationFormServlet.java
@@ -0,0 +1,33 @@
+package com.adatech.adatechuserregistrationexercise.controller;
+
+import java.io.*;
+
+import com.adatech.adatechuserregistrationexercise.ImprovisedEnumInjector;
+import com.adatech.adatechuserregistrationexercise.service.UserService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "registrationFormServlet", value = "/registration-form")
+public class RegistrationFormServlet extends BaseServlet {
+    private UserService userService = ImprovisedEnumInjector.USER_SERVICE.getUserService();
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher(jspDirectory + "registration.jsp").forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = request.getParameter("name");
+        String cpf = request.getParameter("cpf");
+        userService.addClient(name, cpf);
+
+        request.setAttribute("name", name);
+        request.setAttribute("cpf", cpf);
+
+        request.getRequestDispatcher(jspDirectory + "display-registration-data.jsp").forward(request, response);
+    }
+
+    public void destroy() {
+    }
+}
