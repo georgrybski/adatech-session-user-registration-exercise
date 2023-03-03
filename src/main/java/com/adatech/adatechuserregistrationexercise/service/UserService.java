609bf303bb129bfdb80a51ded476f92e547e7c5b
georgrybski <rybskigeorg@gmail.com>
2023-03-03 16:27:56 -0300

    Adapted UserService HTML strings to string blocks

--- src/main/java/com/adatech/adatechuserregistrationexercise/service/UserService.java
+++ src/main/java/com/adatech/adatechuserregistrationexercise/service/UserService.java
@@ -8,33 +8,39 @@ import java.util.List;
 
 public class UserService {
 
+    private static final String HTML_CLIENT_CARD =
+            """
+               <div class="card">
+                  <div class="container">
+                     <p>
+                        <span class="bold">Nome:</span> %s
+                        <br>
+                        <br>
+                        <span class="bold">CPF:</span> %s
+                     </p>
+                  </div>
+               </div>
+               <br>
+            """;
+
+    private static final String HTML_NO_CLIENTS =
+            """
+               <div class="card">
+                  <div class="center">
+                     <span class="bold"><h2>Nenhum cliente registrado</h2></span>
+                  </div>
+               </div>
+            """;
+
     public String getAllClientsHtmlContent(List<Client> clients) {
-        return !clients.isEmpty()? createHtmlContent(clients) :
-                "  <div class=\"card\">" +
-                "     <div class=\"center\">" +
-                "        <span class=\"bold\"><h2>Nenhum cliente registrado</h2></span>" +
-                "     </div>" +
-                "  </div>";
+        return !clients.isEmpty()? createHtmlContent(clients) : HTML_NO_CLIENTS;
     }
 
     public String createHtmlContent(List<Client> clients) {
-        String[] content = new String[] {
-                ""
+        StringBuilder[] content = new StringBuilder[] {
+                new StringBuilder()
         };
-        clients.forEach(client -> {
-                content[0] +=
-                        "  <div class=\"card\">\n" +
-                                "    <div class=\"container\">\n" +
-                                "      <p>\n" +
-                                "        <span class=\"bold\">Nome:</span> " + client.getName() +
-                                "        <br>\n" +
-                                "        <br>\n" +
-                                "        <span class=\"bold\">CPF:</span> " + client.getCpf() +
-                                "      </p>\n" +
-                                "    </div>\n" +
-                                "  </div>" +
-                                "  <br>\n" ;
-            });
-            return content[0];
+        clients.forEach(client -> content[0].append(HTML_CLIENT_CARD.formatted(client.getName(), client.getCpf())));
+            return content[0].toString();
         }
 }
